96478b22c464616f5cc278ff19cb0c5b75864d2b
vandanafuloria <vandanafuloria02@gmail.com>
2025-03-10 21:19:55 +0530

    added local stroage

--- script.js
+++ script.js
@@ -9,15 +9,17 @@ const pagesEl = document.querySelector('#page');
 const readEl = document.querySelector('#read');
 const bookContainerEl = document.querySelector('.content-box');
 
-const mylibrary = [];
+let mylibrary = [];
 
-function bookInfoConstructor(book, writer, pages, read){
+function BookInfo(book, writer, pages, read){
     this.book = book;
     this.writer = writer;
     this.pages = pages;
     this.read = read;
- 
-}
+    this.id = Date.now(); // every book have a id //
+    }
+
+
 
 function dialogBoxOpen(){
     dialogEl.showModal();
@@ -27,6 +29,88 @@ function closeDialogBox(){
     dialogEl.close();
 }
 
+
+
+
+
+
+
+
+function getBookElement(book){
+    const bookElement = document.createElement('div');
+    bookElement.classList.add('bookInfo')
+    const nameOfBook = document.createElement('h3');
+    nameOfBook.innerText = book.book;
+  
+    
+    const nameOfWriter = document.createElement('p');
+    nameOfWriter.innerText = book.writer;
+    const numberOfPages = document.createElement('h6');
+    numberOfPages.innerText = book.pages;
+
+    bookElement.append(nameOfBook, nameOfWriter, numberOfPages);
+
+    const fileContainerEl = document.createElement('div');
+    fileContainerEl.classList.add('file-container')
+
+
+    const fileUploadEl = document.createElement('input');
+    fileUploadEl.setAttribute("type", 'file');
+    fileUploadEl.setAttribute('id', 'file')
+ 
+  
+    const urlUpload = document.createElement('input');
+    urlUpload.setAttribute('id', 'url');
+    urlUpload.setAttribute('placeholder', 'URL for image')
+    
+    fileContainerEl.append(fileUploadEl,  urlUpload);
+    
+    bookElement.append(fileContainerEl);
+
+    const btnBoxEl = document.createElement('div');
+    btnBoxEl. classList.add('displayButtons');
+    const deleteBookEl = document.createElement('button');
+    deleteBookEl.classList.add("delete");
+    const readBookEl = document.createElement('button');
+    readBookEl.classList.add('read');
+    deleteBookEl.innerText = "Delete";
+    readBookEl.innerText = "Read";
+
+    btnBoxEl.append(readBookEl, deleteBookEl);
+    bookElement.appendChild(btnBoxEl);
+    // bookContainerEl.appendChild(bookElement);
+    console.log(book);
+
+// working properly //
+    deleteBookEl.addEventListener('click', function (event){
+      
+        bookContainerEl.removeChild(bookElement);
+        
+        for(let i = 0; i < mylibrary.length; i++) {
+            if(mylibrary[i].id === book.id) {
+                mylibrary.splice(i, 1);
+                break;
+            }
+        }
+        saveToLocalStorage();
+       
+    })
+
+    return bookElement;
+}
+const urlUpload = document.querySelector('#url');
+const fileUpload = document.querySelector('#file');
+
+const bookElement = document.querySelector(".bookInfo");
+const deleteBookEl = document.querySelector('.delete');
+
+
+function saveToLocalStorage() {
+    const libraryJsonString = JSON.stringify(mylibrary);
+    localStorage.setItem('books', libraryJsonString);    
+}
+
+
 function submitInfo(event){
     event.preventDefault();
     
@@ -35,6 +119,7 @@ function submitInfo(event){
    let writer =writerEl.value;
    let pages = pagesEl.value;
    let read = readEl.checked;
+ 
    if(book == "")
     {
         alert("Enter Book name");
@@ -44,76 +129,58 @@ function submitInfo(event){
     alert("Enter Author Name");
     return;
    } 
-   console.log(book, writer, pages, read);
-   const books = new bookInfoConstructor(book, writer, pages, read);
-   mylibrary.push(book);
-   generateBookDisplay(book, writer, pages, read);
+ 
+   const books = new BookInfo(book, writer, pages, read)
+   mylibrary.push(books);
+   console.log(mylibrary);
+
+   const bookEl = getBookElement(books);
+   bookContainerEl.appendChild(bookEl);
    bookNameEl.value = "";
    writerEl.value = "";
    pages.value = "";
    read= false;
-   
+  
+   closeDialogBox()
+   console.log("saving to local storage: ", mylibrary)
+   saveToLocalStorage();
+  
 }
-console.log(mylibrary);
-
-
-function generateBookDisplay(book, writer, pages, read){
-    const bookElement = document.createElement('div');
-    bookElement.classList.add('bookInfo')
-    const nameOfBook = document.createElement('h3');
-    nameOfBook.innerText = book;
-    
-    const nameOfWriter = document.createElement('p');
-    nameOfWriter.innerText = writer;
-    const numberOfPages = document.createElement('h6');
-    numberOfPages.innerText = pages;
-
-    bookElement.append(nameOfBook, nameOfWriter, numberOfPages);
-    const btnBoxEl = document.createElement('div');
-    btnBoxEl. classList.add('displayButtons');
-    const deleteBookEl = document.createElement('button');
-    deleteBookEl.classList.add("delete");
-    const readBookEl = document.createElement('button');
-    readBookEl.classList.add('read');
-    deleteBookEl.innerText = "Delete";
-    readBookEl.innerText = "Read";
-    btnBoxEl.append(readBookEl, deleteBookEl);
-    bookElement.appendChild(btnBoxEl);
-    bookContainerEl.appendChild(bookElement);
-
-// working properly //
-    deleteBookEl.addEventListener('click', function (){
-        bookContainerEl.removeChild(bookElement);
-    })
-
-    // read function // // need help in this  i got confuse uff;
-    // let toggle = false;
-    // readBookEl.addEventListener('click', function (){
-        
-    //     if(toggle == true){
-    //         readBookEl.innerText = "Read";
-        
-    //     }
-    //     readBookEl.innerText = "Reading..";
-    //      toggle = true;
-
-        
-        
-    // })
- 
-}
-const bookElement = document.querySelector(".bookInfo");
-const deleteBookEl = document.querySelector('.delete');
-
-
-// function delelteBook(){
-//     bookContainerEl.removeChild(bookElement);
-// }
 
 
 submitEl.addEventListener('click', submitInfo);
 
 closeBtnEl.addEventListener('click', closeDialogBox);
 buttonEl.addEventListener('click', dialogBoxOpen);
+
+
+
+function initializeApplication (){
+    const booksJsonString  = localStorage.getItem('books');
+    const parsed = JSON.parse(booksJsonString);
+    mylibrary = parsed;
+    
+    // console.log("initialize");
+    // console.log(parsed);
+    // console.log(parsed.length);
+    for(let book of parsed){
+        const bookEl = getBookElement(book)
+        bookContainerEl.appendChild(bookEl);
+        console.log(book);
+    }
+  
+
+    
+    
+}
+
+window.addEventListener('load', initializeApplication);
+// window.addEventListener('keydown',function(event){
+//       console.log(event.key)
+//     if(event.key === 13 ){
+        
+//        closeDialogBox()
+//     }
+// })
 // deleteBookEl.addEventListener("click", delelteBook);
 
